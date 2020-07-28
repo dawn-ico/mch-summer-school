13aa3c0567cfecec7d1120ee8a2b330f1cd9f9fe
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-07-28 11:43:51 +0200

    switch dusk to master

--- cmake/FetchDusk.cmake
+++ cmake/FetchDusk.cmake
@@ -5,7 +5,7 @@ if(NOT FETCHED_DUSK)
 endif()
 FetchContent_Declare(dusk
   GIT_REPOSITORY git@github.com:dawn-ico/dusk.git
-  GIT_TAG 8e52fe29406cbc5ed6fcda89893ee08238262036
+  GIT_TAG master
 )
 FetchContent_GetProperties(dusk)
 mark_as_advanced(FETCHED_DUSK)
